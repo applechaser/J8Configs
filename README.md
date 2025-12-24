9a193c29d61f391d3ed4b28615e4ef3a7bf022fb
applechaser <appelchaser@gmail.com>
2025-12-24 20:48:07 +0100

    L1a1 uses magazines instead of magwell

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # J5 Mod
 
 mod that contains fixes for all things J5.
+
+currently implemented:
+MX and MXAR rifles only accepts 30rnd mags;
+SOG DLC Australian faction L1A1 only accepts 10 and 20rnd mags;
--- addons/main/CfgWeapons.hpp
+++ addons/main/CfgWeapons.hpp
@@ -21,10 +21,12 @@ class CfgWeapons
 
     //SOG DLC Australian faction L1A1 may only accept 10 and 20rnd mags
     class vn_l1a1_01 : arifle_MX_Base_F {
-        magazineWell[] = {"vn_l1a1_10_mag", "vn_l1a1_10_t_mag", "vn_l1a1_20_mag", "vn_l1a1_20_t_mag"};
+        magazines[] = {"vn_l1a1_10_mag", "vn_l1a1_10_t_mag", "vn_l1a1_20_mag", "vn_l1a1_20_t_mag"};
+        magazineWell[] = {};
     };
     class vn_l1a1_01_gl : arifle_MX_Base_F {
-        magazineWell[] = {"vn_l1a1_10_mag", "vn_l1a1_10_t_mag", "vn_l1a1_20_mag", "vn_l1a1_20_t_mag"};
+        magazines[] = {"vn_l1a1_10_mag", "vn_l1a1_10_t_mag", "vn_l1a1_20_mag", "vn_l1a1_20_t_mag"};
+        magazineWell[] = {};
     };
 
 };
