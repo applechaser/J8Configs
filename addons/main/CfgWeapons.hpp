205768e79ada4507e7a6843a4d3f35f5df56cf03
applechaser <appelchaser@gmail.com>
2026-02-10 17:26:31 +0100

    fix error in l1a1 config

--- addons/main/CfgWeapons.hpp
+++ addons/main/CfgWeapons.hpp
@@ -20,12 +20,9 @@ class CfgWeapons
     };
 
     //SOG DLC Australian faction L1A1 may only accept 10 and 20rnd mags
-    class vn_l1a1_01 : arifle_MX_Base_F {
+    class vn_rifle762;
+    class vn_l1a1_01 : vn_rifle762 {
         magazines[] = {"vn_l1a1_10_mag", "vn_l1a1_10_t_mag", "vn_l1a1_20_mag", "vn_l1a1_20_t_mag"};
-        magazineWell[] = {};
     };
-    class vn_l1a1_01_gl : arifle_MX_Base_F {
-        magazines[] = {"vn_l1a1_10_mag", "vn_l1a1_10_t_mag", "vn_l1a1_20_mag", "vn_l1a1_20_t_mag"};
-        magazineWell[] = {};
-    };
+    class vn_l1a1_01_gl : vn_l1a1_01 {};
 };
